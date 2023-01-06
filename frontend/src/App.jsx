1f1179c1393aa24a6d8d58aa809713e4b6cf69ed
shobhnik13 <shobhnikw@gmail.com>
2023-01-06 22:30:07 +0530

    initialise project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+<h1 className='text-fuchsia-800 bg-black'>hey</h1>
+         </div>
+  )
+}
+
+export default App
