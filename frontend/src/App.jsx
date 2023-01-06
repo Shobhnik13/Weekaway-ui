50dbc116fedd40aa4f90d8f07da409d5facc08a4
shobhnik13 <shobhnikw@gmail.com>
2023-01-07 01:23:37 +0530

    added navbar component and responsive design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Topbar from './Components/Topbar'
+import Navbar from './Components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-<Topbar/>
-
+    <Topbar/>
+    <Navbar/>
          </div>
   )
 }
